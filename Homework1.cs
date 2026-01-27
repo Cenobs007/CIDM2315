c474f64fd9b03b02bdf2163e49986b344287af5d
Cenobs007 <eenobakhare1@buffs.wtamu.edu>
2026-01-26 19:40:59 -0600

    submit homework 1

---
+++ Homework1.cs
@@ -0,0 +1,15 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {        
+            // The linear line Equation is: Z = 4X^2 + 3Y
+            // Given values
+            double X = 2.5;
+            double Y = 3.3;
+            double Z = (4 * X * X + 3 * Y);        
+        Console.WriteLine("The value of Z is: " + Z); // the result
+        Console.WriteLine("Homework 1 Completed!");
+    }
+}
